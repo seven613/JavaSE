a5e5f9ea0e8f35cf08f8c64807781dead16a25fe
seven613 <seven613@126.com>
2022-02-06 09:50:49 +0800

    基础语法--注释

---
+++ 基础语法/src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        //单行注释
+        /*
+         多行注释
+         */
+        /**
+         * JavaDoc 注释
+         */
+        System.out.println("Hello,World");
+    }
+}

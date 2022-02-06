653d58977ad09b47be2dc15c58619e52d2da8aed
seven613 <seven613@126.com>
2022-02-06 20:41:58 +0800

    Java 基础语法

---
+++ 基础语法/src/Demo01.java
@@ -0,0 +1,6 @@
+public class Demo01 {
+    public static void main(String[] args) {
+        String aHello="data";
+        System.out.println("args = " + aHello);
+    }
+}
---
+++ 基础语法/src/Demo2.java
@@ -0,0 +1,24 @@
+public class Demo2 {
+    public static void main(String[] args) {
+        //八大基本数据类型
+
+        //整数
+        int num1=10;
+        byte num2=20;
+        short num3=30;
+        long num4=30L;//long类型要在后面加个L
+
+        //小数
+        float num5=50.1F; //float类型要在数字后面加个F
+        double num6=3.14159265358979;
+
+        //字符
+        char name='过';
+        //字符串,String 不是关键字,是一个类
+        //String name1='水电费撒';
+
+        //布尔值
+        boolean flag =true;
+
+    }
+}
